991493510faa240c8d5e47fe2d7530eb7f2e98c5
Sergey Bur <zacsergey@gmail.com>
2024-01-11 20:44:18 +0100

    Создаст шаблон

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/styles/global.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <div className={"text-4xl"}>Hello world!</div>
+    </React.StrictMode>,
+)
